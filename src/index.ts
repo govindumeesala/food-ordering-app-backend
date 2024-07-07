e814a88946e01a23951e7b6d909ea9adc98fdf88
govindumeesala <meesalagovindu123@gmail.com>
2024-07-07 22:54:32 +0100

    message

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import { v2 as cloudinary } from "cloudinary";
 
 const app = express();
 
@@ -10,6 +12,13 @@ const app = express();
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("connected to database"));
+
+// image from form data handling
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // middleware
 
@@ -19,6 +28,7 @@ app.use(express.json());
 app.use(cors());
 // routes
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.get("/health", (req: Request, res: Response) => {
   res.send({ message: "health OK!" });
--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { body, validationResult } from "express-validator";
 
-const handleVadilatoinErrors = async (
+const handleValidationErrors = async (
   req: Request,
   res: Response,
   next: NextFunction
@@ -29,5 +29,29 @@ export const validateMyUserRequest = [
     .isString()
     .notEmpty()
     .withMessage("Country must be a non empty string"),
-  handleVadilatoinErrors,
+  handleValidationErrors,
+];
+
+export const validateMyRestaurantRequest = [
+  body("restaurantName").notEmpty().withMessage("Restaurant name is required"),
+  body("city").notEmpty().withMessage("City is required"),
+  body("country").notEmpty().withMessage("Country is required"),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .withMessage("Delivery price must be a positive number"),
+  body("estimatedDeliveryTime")
+    .isInt({ min: 0 })
+    .withMessage("Estimated delivery time must be a postivie integar"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be an array")
+    .not()
+    .isEmpty()
+    .withMessage("Cuisines array cannot be empty"),
+  body("menuItems").isArray().withMessage("Menu items must be an array"),
+  body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("Menu item price is required and must be a postive number"),
+  handleValidationErrors,
 ];
