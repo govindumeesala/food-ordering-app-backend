0efb4a1f6ba108356854cbaee40a36d1bebbda5f
govindumeesala <meesalagovindu123@gmail.com>
2024-09-11 00:55:22 +0530

    vercel configuration

--- api/index.ts
+++ api/index.ts
@@ -2,48 +2,49 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import myUserRoute from "./routes/MyUserRoute";
-import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import myUserRoute from "../src/routes/MyUserRoute";
+import myRestaurantRoute from "../src/routes/MyRestaurantRoute";
 import { v2 as cloudinary } from "cloudinary";
-import restaurantRoute from "./routes/RestaurantRoute";
-import orderRoute from "./routes/OrderRoute";
+import restaurantRoute from "../src/routes/RestaurantRoute";
+import orderRoute from "../src/routes/OrderRoute";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
 const app = express();
 
-// database connection
+// Database connection
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
-  .then(() => console.log("connected to database"));
+  .then(() => console.log("Connected to the database"))
+  .catch((error) => console.error("Database connection error:", error));
 
-// image from form data handling
+// Cloudinary configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// middleware
+// Middleware
 
-// security
+// Security
 app.use(cors());
 
 app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
 
-// converting api req to json
+// Converting API requests to JSON
 app.use(express.json());
 
-// routes
+// Routes
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", restaurantRoute);
 app.use("/api/order", orderRoute);
 
 app.get("/health", (req: Request, res: Response) => {
-  res.send({ message: "health OK!" });
+  res.send({ message: "Health OK!" });
 });
 
-// server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Export the app as a Vercel serverless function handler
+export default (req: VercelRequest, res: VercelResponse) => {
+  app(req, res); // Delegate the request to your Express app
+};
